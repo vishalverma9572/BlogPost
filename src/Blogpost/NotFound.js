fd01dc19c53a48be9b61dd53875070b0b8ff3340
Vishal Kumar <vishalverma9572@gmail.com>
2024-01-04 19:51:46 +0530

    Reafctoring in different js file

--- src/Blogpost/NotFound.js
+++ src/Blogpost/NotFound.js
@@ -1,15 +1,17 @@
 // NotFound.js
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const NotFound = () => {
-  
+  const navigate=useNavigate()
   const notFoundStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '70vh',
+    height: '67vh',
     fontFamily: 'Arial, sans-serif',
     textAlign: 'center',
   };
@@ -26,12 +28,38 @@ const NotFound = () => {
   };
 
   return (
+    <>
     <div style={notFoundStyle}>
       <h2 style={headingStyle}>404 Not Found</h2>
       <p style={paragraphStyle}>
         Sorry, the page you are looking for does not exist.
       </p>
     </div>
+    <div
+            onClick={()=>{
+              navigate('/')
+            }}
+            style={{
+              display: 'flex',
+              margin: '10px auto',
+              justifyContent: 'center'
+            }}
+          >
+            <span
+              style={{
+                padding: '0.5em',
+                border: '1.5px solid black',
+                cursor: 'pointer',
+                borderRadius: '5px',
+                color: '#ff7878',
+                fontSize: '1.1rem',
+                fontWeight: '500'
+              }}
+            >
+              Go To Home
+            </span>
+          </div>
+    </>
   );
 };
 
